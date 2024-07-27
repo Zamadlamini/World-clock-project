32ecff55c01e02a8a39ad7952d26b0a1851a92c1
zamagugu <127425220+Zamadlamini@users.noreply.github.com>
2024-07-28 00:41:40 +0200

    update Display select

--- Java script/Index.js
+++ Java script/Index.js
@@ -3,22 +3,27 @@
                     const timezone = city.getAttribute('data-timezone');
                     const now = moment().tz(timezone);
                     city.querySelector('.date').innerHTML = now.format('MMMM Do YYYY');
-                    city.querySelector('.time').innerHTML = now.format('h:mm:ss') + '<small>' + now.format('A') + '</small>';
+                    city.querySelector('.time').innerHTML = now.format('h:mm:ss') + '&nbsp;<small>' + now.format('A') + '</small>';
                 });
             }
             document.getElementById('city-select').addEventListener('change', function () {
-                const cityDiv = document.createElement('div');
-                cityDiv.className = 'city';
-                cityDiv.setAttribute('data-timezone', selectedTimezone);
-                const now = moment().tz(selectedTimezone);
-                cityDiv.innerHTML = `
-    <div>
-    <h2>${this.options[this.selectedIndex].text}</h2>
-    <div class ="date">${now.format('MMMM Do YYYY')}</div>
-    </div>
-    <div class="time">${now.format('h:mm:ss')}<small>${now.format('A')}</small></div>
-    `;
-                document.getElementById('cities').appendChild(cityDiv);
-            });
-            setInterval(updateTime, 1000);
-            updateTime();
+                const selectedTimezone = this.value;
+                if (selectedTimezone) {
+                    const selectedText = this.options[this.selectedIndex].text;
+                    const now = moment().tz(selectedTimezone);
+                       const cityDiv = `
+                    <div class="city" data-timezone="${selectedTimezone}">
+                        <div>
+                            <h2>${selectedText}</h2>
+                            <div class="date">${now.format('MMMM Do YYYY')}</div>
+                        </div>
+                        <div class="time">${now.format('h:mm:ss')}&nbsp;<small>${now.format('A')}</small></div>
+                    </div>
+                `;
+                document.getElementById('cities').innerHTML = cityDiv;
+                updateTime();
+            }
+        });
+
+        setInterval(updateTime, 1000);
+        updateTime();
